2de4225e0db0894bd7bc2fcad5c7613443b30de8
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-10 15:30:14 +0200

    Add isDate1BeforeDate2 and calculateDifferenceInDays methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.chrono.ChronoLocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LocalTime now = LocalTime.now();
+        LocalDate today = LocalDate.now();
+        LocalDate dateToTest = LocalDate.parse("2023-12-22");
+
+        System.out.println(now);
+        System.out.println(today);
+
+        System.out.println(today.plusWeeks(2));
+
+        isDate1BeforeDate2(today,dateToTest);
+        System.out.println(calculateDifferenceInDays(today,dateToTest));
+
+
+    }
+
+    public static boolean isDate1BeforeDate2(LocalDate date1,LocalDate date2) {
+        if(date1.isBefore(date2)) {
+            System.out.println(date1 + " is before " + date2);
+            return true;
+        }
+        System.out.println(date1 + " is not before " + date2);
+        return false;
+    }
+
+    public static long calculateDifferenceInDays(LocalDate date1, LocalDate date2) {
+        return ChronoUnit.DAYS.between(date1,date2);
     }
 }
